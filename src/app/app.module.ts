5af331124ca780bb6eb9e0b5531c54829f3ccb8a
Lhukas <58892260+Lhukas@users.noreply.github.com>
2023-07-28 15:36:23 +0200

    maj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import { ListEmployeComponent } from './list-employe/list-employe.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EmployeComponent } from './employe/employe.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CalendrierEmployeComponent } from './calendrier-employe/calendrier-employe.component';
-import { CollaborateursComponent } from './collaborateurs/collaborateurs.component';
+
+
 
 @NgModule({
   declarations: [
@@ -20,13 +20,12 @@ import { CollaborateursComponent } from './collaborateurs/collaborateurs.compone
     MenuNavComponent,
     ListEmployeComponent,
     EmployeComponent,
-    CalendrierEmployeComponent,
-    CollaborateursComponent
+   
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   //FR
   providers: [{ provide : LOCALE_ID, useValue : 'fr-FR'}],
--- src/app/calendrier-employe/calendrier-employe.component.ts
+++ src/app/calendrier-employe/calendrier-employe.component.ts
@@ -13,9 +13,21 @@ export class CalendrierEmployeComponent implements OnInit {
   employe!: Employe
   prenom !: string
 
+   currentMonth: number;
+   currentYear: number;
+   daysInMonth!: number[];
+   weeks!: number[][];
+
+
+ viewDate : Date = new Date()
   constructor(private route: ActivatedRoute,
       private employeService :EmployeService
-    ) { }
+    ) {
+      const currentDate = new Date();
+      this.currentMonth = currentDate.getMonth();
+      this.currentYear = currentDate.getFullYear();
+      this.generateCalendar();
+     }
 
   ngOnInit(): void {
     const employe_id = +this.route.snapshot.params['id'];
@@ -23,6 +35,57 @@ export class CalendrierEmployeComponent implements OnInit {
 
 
     this.prenom = this.employe.prenom
+  }
+
+  // Méthode pour générer le calendrier
+  generateCalendar(): void {
+    this.daysInMonth = [];
+    this.weeks = [];
+
+    const firstDayOfMonth = new Date(this.currentYear, this.currentMonth, 1);
+    const lastDayOfMonth = new Date(this.currentYear, this.currentMonth + 1, 0);
+    const daysInMonth = lastDayOfMonth.getDate();
+
+    let dayOfWeek = firstDayOfMonth.getDay();
+    let week: number[] = [];
+
+    for (let i = 1; i <= daysInMonth; i++) {
+      week.push(i);
+      dayOfWeek++;
+
+      if (dayOfWeek === 7 || i === daysInMonth) {
+        this.weeks.push(week);
+        week = [];
+        dayOfWeek = 0;
+      }
+    }
+  }
+
+  // Méthode pour changer le mois
+  changeMonth(monthOffset: number): void {
+    this.currentMonth += monthOffset;
+
+    if (this.currentMonth < 0) {
+      this.currentYear--;
+      this.currentMonth = 11;
+    } else if (this.currentMonth > 11) {
+      this.currentYear++;
+      this.currentMonth = 0;
+    }
+
+    this.generateCalendar();
+  }
+
+  getMonthName(monthIndex: number): string {
+    const monthNames = [
+      'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+      'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
+    ];
+    return monthNames[monthIndex];
+  }
+
+  onDateClick(date: number): void {
+    console.log(date);
   }
 
 }
